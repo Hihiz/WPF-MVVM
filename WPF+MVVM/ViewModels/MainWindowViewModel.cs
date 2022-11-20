8c9cba737b49e551cefdd87d03c90d5378652d63
Hihiz <public@mail.ru>
2022-11-20 18:33:19 +0300

    Тестирование работы модели представления

--- WPF+MVVM/ViewModels/MainWindowViewModel.cs
+++ WPF+MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace WPF_MVVM.ViewModels
+﻿using WPF_MVVM.ViewModels.Base;
+
+namespace WPF_MVVM.ViewModels
 {
-    public class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
-        
+        #region Заголовок окна
+        private string _Title = "Анализ статистики Cov19";
+
+        /// <summary>Заголовок окна</summary> 
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
