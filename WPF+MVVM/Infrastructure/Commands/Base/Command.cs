9b5f94570020833a54c75999dacc42754dc8f1bd
Hihiz <public@mail.ru>
2022-11-21 20:16:49 +0300

    Базовый класс комманды

--- WPF+MVVM/Infrastructure/Commands/Base/Command.cs
+++ WPF+MVVM/Infrastructure/Commands/Base/Command.cs
@@ -1,7 +1,18 @@
-﻿namespace WPF_MVVM.Infrastructure.Commands.Base
-{
-    public class Command
-    {
+﻿using System;
+using System.Windows.Input;
 
+namespace WPF_MVVM.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object? parameter);        
+
+        public abstract void Execute(object? parameter);
     }
 }
