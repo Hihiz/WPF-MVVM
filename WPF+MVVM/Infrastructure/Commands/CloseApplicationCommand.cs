37bba6feb200ec704fdb1eb9ef0e9adaca1d3efc
Hihiz <public@mail.ru>
2022-11-24 00:05:23 +0300

    Определить класс отдельной команды

---
+++ WPF+MVVM/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using WPF_MVVM.Infrastructure.Commands.Base;
+
+namespace WPF_MVVM.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();        
+    }
+}
