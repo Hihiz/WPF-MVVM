7962467cf3f36d8ff35e083542ddd4a943da14dc
Hihiz <public@mail.ru>
2022-12-08 23:19:02 +0300

    Добавление и удаление групп

--- WPF+MVVM/ViewModels/MainWindowViewModel.cs
+++ WPF+MVVM/ViewModels/MainWindowViewModel.cs
@@ -121,6 +121,7 @@ namespace WPF_MVVM.ViewModels
 
         #endregion
 
+        #region ChangeTabIndexCommand
         public ICommand ChangeTabIndexCommand { get; }
         private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
         private void OnChangeTabIndexCommandExecuted(object p)
@@ -128,6 +129,40 @@ namespace WPF_MVVM.ViewModels
             if (p is null) return;
             SelectedPageIndex += Convert.ToInt32(p);
         }
+        #endregion
+
+        public ICommand CreateGroupCommand { get; }
+
+        private bool CanCreateGroupCommandExecute(object p) => true;
+
+        private void OnCreateGroupCommandExecuted(object p)
+        {
+            var group_max_index = Groups.Count + 1;
+            var new_group = new Group
+            {
+                Name = $"Группа {group_max_index}",
+                Students = new ObservableCollection<Student>()
+            };
+
+            Groups.Add(new_group);
+        }
+
+        #region DeleteGroupCommand
+
+        public ICommand DeleteGroupCommand { get; }
+
+        private bool CanDeleteGroupCommandExecuted(object p) => p is Group group && Groups.Contains(group);
+
+        private void OnDeleteGroupCommandExecuted(object p)
+        {
+            if (!(p is Group group)) return;
+            var group_index = Groups.IndexOf(group);
+            Groups.Remove(group);
+            if (group_index < Groups.Count)
+                SelectedGroup = Groups[group_index];
+        }
+
+        #endregion
 
         #endregion
 
@@ -138,6 +173,9 @@ namespace WPF_MVVM.ViewModels
 
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationExecute);
             ChangeTabIndexCommand = new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
+            CreateGroupCommand = new LambdaCommand(OnCreateGroupCommandExecuted, CanCreateGroupCommandExecute);
+            DeleteGroupCommand = new LambdaCommand(OnDeleteGroupCommandExecuted, CanDeleteGroupCommandExecuted);
+
             #endregion
 
             var data_points = new List<DataPoint>((int)(360 / 0.1));
