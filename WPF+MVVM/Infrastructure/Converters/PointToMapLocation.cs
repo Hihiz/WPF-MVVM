20007e7d119b60c28120412ee20ca5e5c1b6630f
Hihiz <public@mail.ru>
2022-12-21 21:33:57 +0300

    Собственная точка входа в приложение - метод Program.Main

--- WPF+MVVM/Infrastructure/Converters/PointToMapLocation.cs
+++ WPF+MVVM/Infrastructure/Converters/PointToMapLocation.cs
@@ -1,11 +1,7 @@
 ﻿using MapControl;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Markup;
 
---
+++ WPF+MVVM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WPF_MVVM
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
