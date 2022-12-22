299d13edbd8e2c34f4fe6293a34e0ffcf798e09a
Hihiz <public@mail.ru>
2022-12-22 20:23:02 +0300

    Блокировка критической секции

--- Tests/WPF+MVVMConsole/Program.cs
+++ Tests/WPF+MVVMConsole/Program.cs
@@ -5,27 +5,42 @@
         static void Main(string[] args)
         {
             Thread.CurrentThread.Name = "Main theread";
-            var thread = new Thread(ThreadMethod);
-            thread.Name = "Other thread";
-            thread.IsBackground = true;
-            thread.Priority = ThreadPriority.AboveNormal;
 
-            thread.Start(42);
+            var values = new List<int>();
 
-            var count = 5;
-            var msg = "Hello World!";
-            var timeout = 150;
+            var threads = new Thread[10];
+            object lock_object = new object();
+            for (var i = 0; i < threads.Length; i++)
+                threads[i] = new Thread(() =>
+                {
+                    for (var j = 0; j < 10; j++)
+                    {
+                        lock (lock_object)
+                            values.Add(Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(1);
+                    }
+                });
 
-            new Thread(() => PrintMethod(msg, count, timeout)) { IsBackground = true }.Start();
-
-            CheckThread();
-
-            for (var i = 0; i < 5; i++)
+            Monitor.Enter(lock_object);
+            try
             {
-                Thread.Sleep(100);
-                Console.WriteLine(i);
+
             }
+            finally
+            {
+                Monitor.Exit(lock_object);
+            }
+
+            foreach (var thread in threads)
+                thread.Start();
+
+
             Console.ReadLine();
+            Console.WriteLine(string.Join(",", values));
+
+            Console.ReadLine();
+
+
         }
 
         private static void PrintMethod(string Message, int Count, int Timeout)
@@ -36,12 +51,10 @@
                 Thread.Sleep(Timeout);
             }
         }
-
         private static void ThreadMethod(object parameter)
         {
             var value = (int)parameter;
             Console.WriteLine(value);
-
             CheckThread();
             while (true)
             {
